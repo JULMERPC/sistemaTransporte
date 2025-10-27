6fe453a12a98cbdf529e59f30e0d2125963f9cd5
Julmer Puma <julmerpumacondori@gmail.com>
2025-10-26 19:01:10 -0500

    backend completo papu

--- ms-venta/src/main/java/com/example/ms/venta/exception/DuplicateEmailException.java
+++
@@ -1,7 +0,0 @@
-package com.example.ms.venta.exception;
-
-public class DuplicateEmailException extends RuntimeException {
-    public DuplicateEmailException(String message) {
-        super(message);
-    }
-}
